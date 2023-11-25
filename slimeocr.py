91ccc24b85db1be1dcb48d933d65a69b41ca5825
TheRealPlagiariser <shujaatkhanumarkhan@gmail.com>
2023-11-25 05:34:33 +0400

    refactored and added get_loot

--- slimeocr.py
+++ slimeocr.py
@@ -82,21 +82,38 @@ def found_image_icon(image_icon, click_all_instances=False):
 def monitor_screen_for_images(interval_monitoring=5.0):
     try:
         while True:
-            found_image_icon(idle_chest_icon)
-            found_image_icon(obtain_bonus_icon)
-            found_image_icon(ok_main_button_icon)
-
-            if found_image_icon(free_button_icon):
-                found_image_icon(ok_after_free_button_icon)
-                found_image_icon(ok_main_button_icon)
-
-            if found_image_icon(blessings_icon):
-                found_image_icon(receive_blessing, click_all_instances=True)
-                found_image_icon(close_blessing_button)
+            get_idle_chest()
+            get_blessings()
+            get_loot()
 
             time.sleep(interval_monitoring)
     except KeyboardInterrupt:
         print("Monitoring stopped.")
+
+def get_idle_chest():
+    if found_image_icon(idle_chest_icon):
+        if found_image_icon(obtain_bonus_button):
+            if found_image_icon(free_button_icon):
+                found_image_icon(ok_after_free_button_icon)
+        found_image_icon(ok_main_button_icon)
+
+def get_blessings():
+    if found_image_icon(blessings_icon):
+        found_image_icon(receive_blessing, click_all_instances=True)
+        found_image_icon(close_blessing_button)
+
+def get_loot():
+    if found_image_icon(building_icon):
+        if found_image_icon(loot_button):
+            if found_image_icon(receive_all_button):
+                found_image_icon(tap_to_continue)
+            if found_image_icon(production_boost_enabled):
+                # missing add hours icon
+                found_image_icon(close_production_boost_button)
+            # missing production_boost_disabled icon
+        if found_image_icon(loot_back_button):
+            found_image_icon(building_close_button)
+     
 
 def image_path(filename):
     return f"{IMAGE_PATH}/{filename}"
@@ -105,12 +122,21 @@ def image_path(filename):
 time.sleep(2.0)
 
 idle_chest_icon = image_path("idle_chest.png")
-obtain_bonus_icon = image_path("obtain_bonus.png")
+obtain_bonus_button = image_path("obtain_bonus_button.png")
 free_button_icon = image_path("free_button.png")
 ok_main_button_icon = image_path("ok_main_button.png")
 ok_after_free_button_icon = image_path("ok_after_free_button.png")
 blessings_icon = image_path("blessings.png")
 receive_blessing = image_path("receive_blessing.png")
 close_blessing_button = image_path("close_blessing_button.png")
+building_icon = image_path("building.png")
+loot_button = image_path("loot_button.png")
+receive_all_button = image_path("receive_all_button.png")
+tap_to_continue = image_path("tap_to_continue.png")
+production_boost_enabled = image_path("production_boost_enabled.png")
+close_production_boost_button = image_path("close_production_boost_button.png")
+loot_back_button = image_path("loot_back_button.png")
+building_close_button = image_path("building_close_button.png")
+
 
 monitor_screen_for_images(interval_monitoring=10.0)
